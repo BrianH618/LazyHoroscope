c587bc8d189554ef34e4430d622e5599c852d748
brianh618 <bherrera10@mail.csuchico.edu>
2018-02-15 11:41:46 -0800

    this ask for your name and gives you a fortune

--- main.cpp
+++ main.cpp
@@ -7,8 +7,11 @@ using namespace std;
 int main()
 {
   //declare variables
+    int lucky;
     string first;
     string lastname;
+    int len;
+
   //get user input
   cout<< "What is your First name?\n";
   cin>> first;
@@ -18,10 +21,38 @@ int main()
     
   string word1 = first;
   string word2 = lastname;
+  char letter = first[0];
   cout<< "Welcome, "<<word1[0] <<"."<<word2[0]<<"." <<" here is your fortune...";
-
   cout<<endl;
   //tell fortune
+  cout<< "Your first name has "<<first.length()<<" letters"<<endl;
+  
+  lucky = first.length();
+  
+  len = lastname.length();
+  
+  cout<< "your lucky number is "<< lucky<<endl;
+  
+  if (
+  word1[0] == 'a'|| word1[0] =='e'
+  ||word1[0] =='i'||word1[0] =='o'
+  ||word1[0] =='A'||word1[0] =='E'||word1[0] =='I'
+  ||word1[0] =='O'||word1[0] =='u'||word1[0] =='U'
+  )
+  {
+    cout<<"you are destined to be famous!\n";
+  }
+  else 
+  {
+    cout<<"you should keep a low profile.\n";
+  }
+  
+  if(word2[len-1]== 'a' || word2[len-1]== 'e' || word2[len-1]== 'i' || word2[len-1]== 'o' || word2[len-1]=='u' 
+  ||  word2[len-1]=='A' || word2[len-1]=='E' || word2[len-1]=='I' || word2[len-1]== 'O' || 'U');
+  {
+    cout<<"you have already met your true love.\n";
+  }
+  cout<<"have a good day\n";
 
   return 0;
 }
